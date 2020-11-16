8c9f42a40b0f2aad2d9b2a195c0023941d306c6b
WELL ALKLINE KNOWLEDGE <68064582+mrspyder007@users.noreply.github.com>
2020-11-15 20:47:41 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScanScreen from './screens/ScanScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ScanScreen />
+    )
+  }
+}
+
+
